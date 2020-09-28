b6cf1edc10a87967727f0414f64d9f7be4211644
Egor Zakharov <22093374+egorzakharov@users.noreply.github.com>
2020-09-28 14:30:02 +0300

    Update pose_matching.py

--- losses/pose_matching.py
+++ losses/pose_matching.py
@@ -8,7 +8,6 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 import sys
-sys.path.append('/group-volume/orc_srr/violet/e.zakharov/projects/face-alignment')
 import face_alignment
 
 # This project
